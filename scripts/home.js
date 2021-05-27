2d9d36264f88299ea181592696797cc79bf48f6d
Aniruddha Pandey <anirudh.pandev@gmail.com>
2021-05-27 12:16:36 +0530

    update

--- scripts/home.js
+++ scripts/home.js
@@ -6,7 +6,7 @@
 const recommendedCarsId = JSON.parse(localStorage.getItem('recommendedCarsId'))
 
 if (recommendedCarsId) {
-	console.log('does not exists')
-} else {
 	console.log('exists', recommendedCarsId)
+} else {
+	console.log('does not exists')
 }
