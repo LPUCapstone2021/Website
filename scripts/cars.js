999bf2da8ac165446e144012a36a7612e6676374
Aniruddha Pandey <anirudh.pandev@gmail.com>
2021-05-27 19:20:00 +0530

    car cads

--- scripts/cars.js
+++ scripts/cars.js
@@ -1,13 +1,13 @@
 const recommendedCarsId = JSON.parse(localStorage.getItem('recommendedCarsId'))
 
-let card = `
+let card = ({name}) => `
 	<div class="card container">
 	  <div class="card-body">
 	     <div class="car-img col-sm-4" >
 	        <img src="/images/logo.png" alt="carimg">
 	     </div>
 	     <div class="col-sm-5">
-	        <h3>Name of the car</h3>
+	        <h3>${name}</h3>
 	        <p>little bit details about the car</p>
 	        <h4>Price Rs. 4 - 6 Lack</h4>
 	        <p>Milege | Seating space</p>
@@ -22,8 +22,14 @@ let card = `
 
 if (recommendedCarsId) {
 	let section = ``
-	recommendedCarsId.forEach(car => section += card)
-	$('section').append( section );
+	let params = new URLSearchParams(recommendedCarsId.map((s, i)=>[`id${i+1}`,s])).toString()
+	fetch(`http://127.0.0.1:5000/cars?${params}`)
+		.then(res => res.json())
+		.then(cars => {
+			cars.forEach(car => section += card(car["Car names"]))
+			$('section').append(section)
+		})
+		.catch(err => console.warn(err))
 } else {
 	console.log('does not exists')
 }
